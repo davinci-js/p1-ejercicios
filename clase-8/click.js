ca7910ef768426d53961be157f7fe5844d875413
afonso <afonsoarrudagesualdi@MacBook-Air-de-Afonso.local>
2024-09-17 22:29:09 -0300

    nuevas clase ejemplo

--- clase-8/click.js
+++ clase-8/click.js
@@ -28,5 +28,28 @@ function cargarAlumno(){
 }
 
 function mostrarLista(){
+    /**
+     * <h1>${alumno}</h1>
+     * <h2>Materias<h2>
+     * 5
+     */
+    //Traigo el primer elemento que sea de clase container
+    const div = document.querySelector('.container');
 
+    //limpio HTML
+    div.innerHTML = "";
+    // Recorro la lista de alumnos
+
+    for(let alumno of alumnos){
+        //Agrego h1 con nombre del alumno
+        div.innerHTML += `<h1>${alumno.nombre}</h1>`;
+        div.innerHTML += `<h2>Materias</h2>`;
+        //div.innerHTML += `<ul class="list-group">`
+        let str = `<ul class="list-group">`;
+        //recorra las materias del alumno
+        for(let materia of alumno.materias){
+            // Armo el li
+            str += `<li class="list-group-items">${materia.nombre} - ${materia.nota}</li>\n`
+        }
+    }
 }
